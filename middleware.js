1df65d3f6cf7d51e172e9414fc8c7eb8bf7ef19b
Sez Sezer <sezsezer@Mac.lan>
2026-05-17 21:48:12 -0700

    Fix middleware matcher - exclude property pages

--- middleware.js
+++ middleware.js
@@ -31,5 +31,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+  matcher: ['/dashboard/:path*', '/api/cron/:path*'],
 }
