1c650ece216b053a1ffff1a8473098b2911b8fa4
Sez Sezer <sezsezer@Sezs-MacBook-Pro.local>
2026-05-18 14:41:29 -0700

    Debug listings v3

--- app/api/debug-listings/route.js
+++ app/api/debug-listings/route.js
@@ -10,11 +10,11 @@ export async function GET() {
   const results = {}
 
   const tests = [
-    'properties/search-sale?regionId=16904&regionType=6&limit=3',
     'properties/search-sale?regionId=16904&regionType=2&limit=3',
-    'properties/search-sale?regionId=16904&regionType=6&limit=3&sort=1',
-    'properties/search-sale?regionId=16904&regionType=6&num=3',
-    'properties/search-sale?region_id=16904&region_type=6&limit=3',
+    'properties/search-sale?regionId=16904&regionType=6&limit=3',
+    'properties/search-sale?regionId=339&regionType=5&limit=3',
+    'properties/search-sale?regionId=16904&regionType=2&limit=3&sort=1&soldWithin=',
+    'properties/search-sale?regionId=16904&regionType=2&num_homes=3',
   ]
 
   for (let i = 0; i < tests.length; i++) {
@@ -24,10 +24,15 @@ export async function GET() {
       results[`test${i+1}`] = {
         url: tests[i],
         status: r.status,
-        keys: Object.keys(data?.data || {}),
-        errors: data?.errors,
-        homes_count: data?.data?.homes?.length || 0,
         message: data?.message,
+        errors: data?.errors,
+        data_keys: Object.keys(data?.data || {}),
+        homes_count: data?.data?.homes?.length || 0,
+        sample: data?.data?.homes?.[0] ? {
+          address: data.data.homes[0].streetLine,
+          price: data.data.homes[0].price,
+          beds: data.data.homes[0].beds,
+        } : null
       }
     } catch(e) { results[`test${i+1}_error`] = e.message }
   }
